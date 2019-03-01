3dbf8aff91debdd8e28b3282969d223b8fd9b5c1
Eugene Korobeinikov <evgenij.k.wie@gmail.com>
2019-03-01 12:34:14 +0300

    max-heap/feat: add some implementation

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,11 +2,14 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
@@ -22,27 +25,47 @@ class MaxHeap {
 	}
 
 	size() {
-		
+		return (this.root == null) ? 0 : this.parentNodes.length + 1;
 	}
 
 	isEmpty() {
-		
+		return !!this.size(); 
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if (this.root == null) {
+			this.root = node;
+		}
+		else {
+			this.parentNodes.push(node);
+		}
+		return;
 	}
 
 	shiftNodeUp(node) {
-		
+		if (node.parent && (node.parent.priority < node.priority)) {
+			node.swapWithParent();
+			this.shiftNodeUp(node);
+		}
 	}
 
 	shiftNodeDown(node) {
-		
+		let maxPriorityNode = node.priority;
+		if (node.left && (maxPriorityNode < node.left.priority)) {
+			maxPriorityNode = node.left;
+		}
+		if (node.right && (maxPriorityNode < node.right.priority)) {
+			maxPriorityNode = node.right;
+		}
+		if (maxPriorityNode != node) {
+			maxPriorityNode.swapWithParent();
+			shiftNodeDown(node);
+		}
 	}
 }
 
