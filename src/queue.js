4b5cc526db9b87f4c7194533af7b217946491945
evgenijkorob <evgenij.k.wie@gmail.com>
2019-03-03 14:13:30 +0300

    queue/feat: add implementation of methods

--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,31 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
+	constructor(maxSize = 30) {
+		this.maxSize = maxSize;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() >= this.maxSize) {
+			throw new Error('Queue is full.');
+		}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw new Error('Queue is empty.');
+		}
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
